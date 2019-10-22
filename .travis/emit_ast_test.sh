c810bf87ba12ef99712fd8bef91c84d0e91623b6
Naga Nithin Manne <nithinmanne@gmail.com>
2019-10-21 21:33:14 -0500

    Create emit_ast_test.sh

---
+++ .travis/emit_ast_test.sh
@@ -0,0 +1,4 @@
+python ekcc.py -emit-ast -o test/test1.ek.ast.yaml test/test1.ek
+python -c "import sys,yaml;sys.exit(yaml.load(open('test/test1.ast.yaml'))!=yaml.load(open('test/test1.ek.ast.yaml')))"
+python ekcc.py -emit-ast -o test/test2.ek.ast.yaml test/test2.ek
+python -c "import sys,yaml;sys.exit(yaml.load(open('test/test2.ast.yaml'))!=yaml.load(open('test/test1.ek.ast.yaml')))"
