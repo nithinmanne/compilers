6765d091cd8635f020ecb8d0b67f27b2de9b63f7
Naga Nithin Manne <nithinmanne@gmail.com>
2019-10-21 21:40:32 -0500

    Update emit_ast_test.sh

--- .travis/emit_ast_test.sh
+++ .travis/emit_ast_test.sh
@@ -1,4 +1,3 @@
 python ekcc.py -emit-ast -o test/test1.ek.ast.yaml test/test1.ek
-python -c "import sys,yaml;sys.exit(yaml.load(open('test/test1.ast.yaml'))!=yaml.load(open('test/test1.ek.ast.yaml')))"
 python ekcc.py -emit-ast -o test/test2.ek.ast.yaml test/test2.ek
-python -c "import sys,yaml;sys.exit(yaml.load(open('test/test2.ast.yaml'))!=yaml.load(open('test/test1.ek.ast.yaml')))"
+python -c "import sys,yaml;sys.exit(yaml.load(open('test/test1.ast.yaml'))!=yaml.load(open('test/test1.ek.ast.yaml')) or yaml.load(open('test/test2.ast.yaml'))!=yaml.load(open('test/test2.ek.ast.yaml')))"
